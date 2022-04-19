7fa56fb03a0a8013205baf677b06f7ec8ab52ae8
Diego Salvio <diantonio46@gmail.com>
2022-04-19 20:35:33 -0300

    Foi implementado um botão que gera 6 números aleatórios que não se repetem

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import React, {useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { render } from 'react-dom';
+import React from "react";
 
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+ 
+
+function aleatorio() {
+  let numero = [];
+  function geraAleatorio() {
+    let num = (Math.random() * 60 + 1).toFixed();
+    return num;
+  }
+  for (let i = 0; i < 6; i++) {
+    let aleatorio = geraAleatorio();
+    numero.push(aleatorio);
+  }
+  let arrayUnica = numero.filter(function (atual, ultimo) {
+    return numero.indexOf(atual) == ultimo;
+  });
+
+  while (arrayUnica.length < 6) {
+    arrayUnica.push(geraAleatorio());
+  }
+  return arrayUnica;
+}
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      contador: 0
-    }
+      outPut: aleatorio(),
+    };
   }
-  render(){
+  render() {
     return (
       <View style={styles.container}>
-        <Text>{this.state.contador}</Text>
-       <Button title="Alterar texto" onPress={()=>{this.setState({contador:this.state.contador + 1})}}/>
+        <Text>1º número:  {this.state.outPut[0]}</Text>
+        <Text>2º número:  {this.state.outPut[1]}</Text>
+        <Text>3º número:  {this.state.outPut[2]}</Text>
+        <Text>4º número:  {this.state.outPut[3]}</Text>
+        <Text>5º número:  {this.state.outPut[4]}</Text>
+        <Text>6º número:  {this.state.outPut[5]}</Text>
+        <Button
+          title="megasena"
+          onPress={() => {
+            this.setState({
+              outPut: (this.outPut = aleatorio()),
+            });
+          }}
+        />
       </View>
     );
   }
@@ -23,8 +54,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
